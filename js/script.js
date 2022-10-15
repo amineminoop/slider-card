0b920c7c9522ae8f02d27c146e52944f93f76443
amineminoop <31425930+amineminoop@users.noreply.github.com>
2022-10-15 21:50:21 +0100

    update script.js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,7 @@ var swiper = new Swiper(".slide-content", {
     fade: 'true',
     grabCursor: 'true',
     loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets:true
-    },
+   
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
